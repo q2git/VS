e21390011f9fca2e2e8d9167778a116e8c208c56
q2git <54710@sohu.com>
2017-06-30 17:11:25 +0800

    Update MainWindow.xaml.cs

--- C#/TextEditor/TextEditor/MainWindow.xaml.cs
+++ C#/TextEditor/TextEditor/MainWindow.xaml.cs
@@ -13,5 +13,35 @@ namespace TextEditor
             if (_documentManager.OpenDocument())
                 status.Text = "Document loaded.";
         }
+
+        private void TextEditorToolbar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (toolbar.IsSynchronizing) return;
+
+            ComboBox source = e.OriginalSource as ComboBox;
+            if (source == null) return;
+
+            switch (source.Name)
+            {
+                case "fonts":
+                    //change the font face
+                    _documentManager.ApplyToSelection(TextBlock.
+                        FontFamilyProperty, source.SelectedItem);
+                    break;
+                case "fontSize":
+                    //change the font size
+                    _documentManager.ApplyToSelection(TextBlock.
+                        FontSizeProperty, source.SelectedItem);
+                    break;
+            }
+            body.Focus();
+        }
+
+        private void body_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            //update the tool bar
+            toolbar.SynchronizeWith(body.Selection);
+            
+        }
     }
 }
